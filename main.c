513a8e6e6ec2d28e46b4fa9471f1f5376a0f2957
MAGI <TATQAQTAT@gmail.com>
2013-11-25 02:38:58 +0800

    fix avrdude statement
    in Code::Blocks

--- main.c
+++ main.c
@@ -12,14 +12,14 @@ int main(void)
 
     // Insert code
 
-    SPI595Out(0x5a);
-    SPI595Out(0x5a);
+    SPI595Out(0xdd);
 
     while(1)
     {
-        //SPI595Out(0x55);
+    //    SPI595Out(0x04);
         //PORTB=~PORTB;
     }
+
 
     return 0;
 }
@@ -29,10 +29,10 @@ int main(void)
 void SPI_MasterInit(void)
 {
 	/*设置SS,MOSI和SCK为输出，其他为输入*/
-	DDRB |= (1<<PB3)|(1<<PB5)|(1<<PB2);
+	DDRB |= (1<<PB3)|(1<<PB5)|(1<<PB0)|(1<<PB2);;
 	//PORTB &= ~(1<<PB2);
 	/* SPI主机，fck/4 .模式0*/
-	SPCR = (1<<SPE)|(1<<MSTR)|(0<<SPR0)|(0<<SPR1)|(0<<CPOL)|(0<<CPHA);
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPR1)|(0<<CPOL)|(0<<CPHA);
 }
 
 void SPI_MasterTransmit(char cData)
@@ -48,11 +48,11 @@ void SPI595Out(uint8_t i)
 
 {
 
-   PORTB&=~(1<<PB2);             //等待数据传输，595存储寄存器数据不变
+    PORTB&=~(1<<PB0);             //等待数据传输，595存储寄存器数据不变
 
-   SPI_MasterTransmit(i);        //传输数据
+    SPI_MasterTransmit(i);
+    SPI_MasterTransmit(i);        //传输数据
 
-
-   PORTB|=(1<<PB2);              //595移位寄存器的数据进入数据存储寄存器，并在数码管上显示
-
+    PORTB|=(1<<PB0);              //595移位寄存器的数据进入数据存储寄存器，并在数码管上显示
+    PORTB&=~(1<<PB0);
 }

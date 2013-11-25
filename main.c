b97943b1306493daad97420ed305815f0ca981e4
MAGI <TATQAQTAT@gmail.com>
2013-11-25 18:27:16 +0800

    add nixietube display function

--- main.c
+++ main.c
@@ -6,18 +6,26 @@ ATMega328P
 #include <avr/delay.h>
 
 int main(void)
-{
+{   unsigned char num=0;
+    unsigned char testplus;
     SPI_MasterInit();
 
 
     // Insert code
 
-    SPI595Out(0xdd);
+    //SPI595Out(0x02);
+    NixieTube(8);
 
     while(1)
     {
-    //    SPI595Out(0x04);
+        //SPI595Out();
         //PORTB=~PORTB;
+        for(num=0;num<10;num++)
+        {
+
+            NixieTube(num);
+            _delay_ms(1000);
+        }
     }
 
 
@@ -32,7 +40,7 @@ void SPI_MasterInit(void)
 	DDRB |= (1<<PB3)|(1<<PB5)|(1<<PB0)|(1<<PB2);;
 	//PORTB &= ~(1<<PB2);
 	/* SPI主机，fck/4 .模式0*/
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPR1)|(0<<CPOL)|(0<<CPHA);
+	SPCR = (1<<SPE)|(1<<MSTR)|(0<<SPR0)|(0<<SPR1)|(0<<CPOL)|(0<<CPHA);
 }
 
 void SPI_MasterTransmit(char cData)
@@ -50,9 +58,25 @@ void SPI595Out(uint8_t i)
 
     PORTB&=~(1<<PB0);             //等待数据传输，595存储寄存器数据不变
 
-    SPI_MasterTransmit(i);
     SPI_MasterTransmit(i);        //传输数据
 
+    PORTB|=(1<<PB0);              //595移位寄存器的数据进入数据存储寄存器，并在数码管上显示
+    PORTB&=~(1<<PB0);
+}
+
+void NixieTube(unsigned char num)
+{
+    PORTB&=~(1<<PB0);             //等待数据传输，595存储寄存器数据不变
+    if(num<8)
+    {
+        SPI_MasterTransmit(0);
+        SPI_MasterTransmit(1<<num);
+    }
+    else
+    {
+        SPI_MasterTransmit(1<<(num-8));//传输数据
+        SPI_MasterTransmit(0);
+    }
     PORTB|=(1<<PB0);              //595移位寄存器的数据进入数据存储寄存器，并在数码管上显示
     PORTB&=~(1<<PB0);
 }
